b2b4d7937706e69ad405ae694bec7a51c2e899b5
febiasm <trevorfebias@gmail>
2023-04-13 16:12:09 +0300

    fixed underline error

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -57,7 +57,7 @@ function Login() {
               Log In
             </button>
 
-            <button className="block text-sm text-center text-gray-700 hover:underline mt-4">
+            <button className="block text-sm text-center text-gray-700  mt-4">
               Don't have an account?{" "}
               <button className="text-blue-500 hover:underline">
                 Create here.
