cf0f1c6d7590548e61c57c6b0ad02327fd6e3a39
febiasm <trevorfebias@gmail>
2023-04-13 22:56:39 +0300

    created landing page

---
+++ frontend/src/components/LandingPage.js
@@ -0,0 +1,47 @@
+function LandingPage() {
+  return (
+    <section style={{ backgroundColor: '#0E503F' }} className="min-h-screen bg-green-900">
+  <div className="flex flex-wrap items-center">
+    <div className="w-full md:w-1/2 px-4">
+      <div className="text-white font-serif ml-64 mt-40  text-6xl">
+      <div className="font-extrabold"> No.1 Trusted <br /> source for <br /></div>  <div className="text-yellow-400 ">Giving <br /> and Volunteering</div>
+      </div>
+    </div>
+    <div className="w-full md:w-1/2 px-4">
+      <img
+
+        className="mx-auto mt-32 h-96 mr-96   "
+        src="https://images.pexels.com/photos/11834966/pexels-photo-11834966.jpeg?cs=srgb&dl=pexels-safari-consoler-11834966.jpg&fm=jpg"
+        alt="description"
+      />
+    </div>
+  </div>
+  <div className="flex flex-wrap  ">
+    <div style={{ width: '53%' }} className="w-full mt-11 px-4">
+      <div className="text-white font-serif ml-64    text-3xl">
+      <div className=""> 
+      We connect nonprofits, donors and companies in nearly every country in the world. 
+      We help fellow nonrofits access the funding,
+      tools,training and support they need to serve their communities. 
+      </div>
+        <div className="flex">
+            <button className="text-white px-4 py-2 rounded-lg hover:bg-green-600 bg-yellow-500">jjj</button>
+            <div className="bg-green-400  text-white px-4 py-2 rounded-lg hover:bg-green-600"> <button className="">hhh</button>
+        </div>
+           </div>
+      </div>
+    </div>
+    <div style={{ width: '47%' }} className="w-full  px-4">
+      <img
+        style={{ width: '700px' }}
+        className="mx-auto mt-7 h-96"
+        src="https://images.pexels.com/photos/11025027/pexels-photo-11025027.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt="description"
+      />
+    </div>
+  </div>
+</section>
+
+  );
+}
+export default LandingPage;
