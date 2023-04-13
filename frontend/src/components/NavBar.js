7e0055213db46535b335a818be8bc15d00260ad2
Fleur41 <Muriithikings456@gmail.com>
2023-04-13 14:10:26 +0300

    Updated  Navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -3,7 +3,7 @@ import React from 'react'
 function NavBar() {
   return (
     
-<div className="     mx-auto">
+<div className="mx-auto">
     
     <nav className="border-gray-200">
     <div className="container mx-auto flex flex-wrap items-center justify-between">
@@ -39,15 +39,15 @@ function NavBar() {
             </li>
 
             <li>
-                <button className='bg-green-300 text-white'>Donate Now</button>
+                <button className='bg-green-300 text-white py-2 block px-4'>Donate Now</button>
             </li>
-                        
+
         </ul>
         </div>
     </div>
     </nav>
 
-    <p class="mt-5">This dropdown navbar element is part of a larger, open-source library of Tailwind CSS components. Learn more by going to the official <a class="text-blue-600 hover:underline" href="#" target="_blank">Flowbite Documentation</a>.</p>
+    
 </div>
 
 
