eb1c6bcff08b868adf6afd080dd5be3a54698175
Fleur41 <Muriithikings456@gmail.com>
2023-04-13 13:54:36 +0300

    Added button to Navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -38,7 +38,10 @@ function NavBar() {
             <a href="#" className="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">Pages  </a>
             </li>
 
-            
+            <li>
+                <button className='bg-green-300 text-white'>Donate Now</button>
+            </li>
+                        
         </ul>
         </div>
     </div>
