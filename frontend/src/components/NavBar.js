8d278907f4de2782d4da1d18b542ebfc03056b48
Fleur41 <Muriithikings456@gmail.com>
2023-04-14 15:29:38 +0300

    Updated my footer

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -33,14 +33,37 @@ function NavBar() {
             <li>
             <a href="#" className="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">Our Campaigns</a>
             </li>
+            
 
             <li>
             <a href="#" className="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">Pages  </a>
             </li>
 
+
+            <li>
+            <div class="relative flex ">
+                <div class="relative rounded-2l">
+                    <div class="mx-auto max-w-md">
+
+
+                        <form action="" class="relative mx-auto w-max">
+                            <input type="search" 
+                                class="peer cursor-pointer relative z-10 h-12 w-12 rounded-full border bg-transparent pl-12 outline-none focus:w-full focus:cursor-text focus:border-lime-300 focus:pl-16 focus:pr-4" />
+                            <svg xmlns="http://www.w3.org/2000/svg" class="absolute inset-y-0 my-auto h-8 w-12 border-r border-transparent stroke-gray-500 px-3.5 peer-focus:border-lime-300 peer-focus:stroke-lime-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                            </svg>
+                        </form>
+
+
+                    </div>
+                </div>
+            </div>
+            </li>
+
             <li>
                 <button className='bg-green-300 text-white py-2 block px-4'>Donate Now</button>
             </li>
+
 
         </ul>
         </div>
