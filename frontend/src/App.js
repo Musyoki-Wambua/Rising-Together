7e35c717bcb788010fd482c309a1b545afe51e6d
Fleur41 <Muriithikings456@gmail.com>
2023-04-14 17:49:13 +0300

    Updating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
-
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className=" App">
-     
+     <NavBar/>
+     <Footer/>
     </div>
   );
 }
