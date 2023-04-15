94c99498c1a0651bc69a13194a2bbad0e67d57b3
faithkigen <kigenfaith20@gmail.com>
2023-04-16 00:01:08 +0300

    added controllers

--- backend/app/controllers/volunteers_controller.rb
+++ backend/app/controllers/volunteers_controller.rb
@@ -1,2 +1,26 @@
 class VolunteersController < ApplicationController
+    before_action :authenticate_volunteer!, except: [:index, :show]
+
+    #GET/ volunteers
+    def index
+        volunteer = Volunteer.All 
+        render :json: volunteers 
+    end
+# GET/volunteers/:id
+    def show
+        volunteer = Volunteer.find_by(id: params[:id])
+        if volunteer
+            render json: volunteer
+        else
+            render :json {error: "Volunteer not found"}
+        end
+
+    end
+
+    # POST/ volunteers
+    def create 
+        volunteer = Volunteer.new(volunteer_params)
+        if volunteer.save
+            render json: volunteer, status: :created
+    end
 end
