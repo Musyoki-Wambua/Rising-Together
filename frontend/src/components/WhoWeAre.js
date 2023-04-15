e0d342090db16b408aade0aa5771aac4913e92f4
Fleur41 <Muriithikings456@gmail.com>
2023-04-15 13:17:36 +0300

    Added more images

--- frontend/src/components/WhoWeAre.js
+++ frontend/src/components/WhoWeAre.js
@@ -7,6 +7,19 @@ function WhoWeAre() {
         <div className="max-w-3xl mx-auto text-center pb-12">
             <h2 className="text-3xl font-bold text-gray-800">Who We Are</h2>
             <p className="mt-4 text-gray-600">
+                We are a team of dedicated individuals who strive to make a positive
+                impact in the world. Our mission is to help those in need and create
+                a better future for generations to come.
+            </p>
+            </div>
+        <div className="flex flex-wrap -mx-4">
+          <div className="lg:w-1/3 px-4 mb-8">
+            <div className="rounded-lg h-64 overflow-hidden">
+              <img
+                className="object-cover h-full w-full"
+                src="https://images.unsplash.com/photo-1579788598106-28d9c9f46bb8"
+                alt="Team member"
+              />
   )
 }
 
