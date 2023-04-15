77f45e7ff800605cf06fb26cac1bf17603b8dbcc
Fleur41 <Muriithikings456@gmail.com>
2023-04-15 13:16:11 +0300

    Uploaded more files

--- frontend/src/components/WhoWeAre.js
+++ frontend/src/components/WhoWeAre.js
@@ -4,6 +4,9 @@ function WhoWeAre() {
   return (
     <section className="bg-gray-100">
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        <div className="max-w-3xl mx-auto text-center pb-12">
+            <h2 className="text-3xl font-bold text-gray-800">Who We Are</h2>
+            <p className="mt-4 text-gray-600">
   )
 }
 
