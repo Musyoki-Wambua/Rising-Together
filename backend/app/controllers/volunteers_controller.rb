59dd10312ca0c00bb18b1914354d20af06e04686
faithkigen <kigenfaith20@gmail.com>
2023-04-17 21:29:14 +0300

    added changes to cobtroller file

--- backend/app/controllers/volunteers_controller.rb
+++ backend/app/controllers/volunteers_controller.rb
@@ -1,18 +1,18 @@
 class VolunteersController < ApplicationController
-    before_action :authenticate_volunteer!, except: [:index, :show]
+   
 
     #GET/ volunteers
     def index
-        volunteer = Volunteer.All 
-        render :json: volunteers 
+        render json: Volunteer.all
+        
     end
 # GET/volunteers/:id
     def show
         volunteer = Volunteer.find_by(id: params[:id])
         if volunteer
-            render json: volunteer
+            render json: volunteer, status: :ok
         else
-            render :json {error: "Volunteer not found"}
+            render json: {error: "Volunteer not found"}, status: :not_found 
         end
 
     end
@@ -22,5 +22,20 @@ class VolunteersController < ApplicationController
         volunteer = Volunteer.new(volunteer_params)
         if volunteer.save
             render json: volunteer, status: :created
+        else 
+            render json: {errors: "volunteer.errors.full_messages"}, status: :unprocessable_entity
+        end
+    end
+
+    #DELETE/ volunteers/:id
+    def destroy
+        volunteer = Volunteer.find(params[:id])
+        volunteer.destroy
+        render json: {message: "Volunteer deleted"}
+    end
+
+    private
+    def volunteer_params
+        params.permit(:organization_name, :organization_id, :user_id)
     end
 end
