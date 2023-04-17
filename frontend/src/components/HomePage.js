0dad9c659e9841e9c574225c30ec194013ce2db8
febiasm <trevorfebias@gmail>
2023-04-17 12:21:42 +0300

    added top ngo cardsfrontend/src/components/HomePage.js

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,142 @@
+function HomePage() {
+  return (     
+    <div className="flex flex-wrap">
+    <div className="mt-6 w-full  md:w-1/2 p-">
+    <div className="h-52">
+    <a href="#" style={{ width: '', height:'67.6vh'}} className="mt- group relative block bg-black" >
+         <img
+           alt="Developer"
+           src="https:images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
+           className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+         />
+
+         <div className=" relative p-4 sm:p-6 lg:p-8">
+         <div className="text-white translate-y-11  mt-96  transition-all group-hover:translate-y-0 group-hover:opacity-100  ">
+         <p className="text-gray-600  text-sm">.</p>  
+         <p className="text-gray-400  text-sm mt-20">jjjj</p>  
+            <p className="text-lg mt-">kkkk</p>
+             <div className="mt-4 translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
+              
+               <button className="bg-green-700 w-full h-8 text-xl    text-white">
+                Donte
+               </button>
+             </div>
+            
+           </div>
+         </div>     
+       </a>  </div>
+    </div>
+
+    {/* right content  */}
+    <div className="mt-2   md:w-1/2 p-4 flex   ">
+       
+    <div className="grid grid-cols-2 gap-1 gap-x-6">
+    <div className="">
+    <a href="#" style={{ width: '50vh ', height:'33.6vh'}} className=" group relative block bg-black" >
+         <img
+           alt="Developer"
+           src="https:images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
+           className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+         />
+
+         <div className=" relative p-4 sm:p-6 lg:p-8">
+            
+
+           <div className="mt- sm:mt- lg:mt-44">
+           <div className="text-white translate-y-11   transition-all group-hover:translate-y-0 group-hover:opacity-100  ">
+            <p className="text-gray-400 text-sm">jjjj</p>  
+            <p className="text-lg">kkkk</p>
+             <div className="mt-4 translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
+              
+               <button className="bg-green-700 w-full h-8 text-xl    text-white">
+                Donte
+               </button>
+             </div>
+            </div>
+           </div>
+         </div>     
+       </a>
+       </div> 
+     
+    <div className="">
+    <a href="#" style={{ width: '50vh ', height:'33.6vh'}} className=" group relative block bg-black" >
+         <img
+           alt="Developer"
+           src="https:images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
+           className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+         />
+
+         <div className=" relative p-4 sm:p-6 lg:p-8">
+            
+
+           <div className="mt- sm:mt- lg:mt-44">
+           <div className="text-white translate-y-11   transition-all group-hover:translate-y-0 group-hover:opacity-100  ">
+            <p className="text-gray-400 text-sm">jjjj</p>  
+            <p className="text-lg">kkkk</p>
+             <div className="mt-4 translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
+              
+               <button className="bg-green-700 w-full h-8 text-xl    text-white">
+                Donte
+               </button>
+             </div>
+            </div>
+           </div>
+         </div>     
+       </a> </div>
+    <div className=" ">
+    <a href="#" style={{ width: '50vh ', height:'33.6vh'}} className=" group relative block bg-black" >
+         <img
+           alt="Developer"
+           src="https:images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
+           className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+         />
+
+         <div className=" relative p-4 sm:p-6 lg:p-8">
+            
+
+           <div className="mt- sm:mt- lg:mt-44">
+           <div className="text-white translate-y-11   transition-all group-hover:translate-y-0 group-hover:opacity-100  ">
+            <p className="text-gray-400 text-sm">jjjj</p>  
+            <p className="text-lg">kkkk</p>
+             <div className="mt-4 translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
+              
+               <button className="bg-green-700 w-full h-8 text-xl    text-white">
+                Donte
+               </button>
+             </div>
+            </div>
+           </div>
+         </div>     
+       </a> </div>
+    <div>
+    <a href="#" style={{ width: '50vh ', height:'33.6vh'}} className=" group relative block bg-black" >
+         <img
+           alt="Developer"
+           src="https:images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
+           className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+         />
+
+         <div className=" relative p-4 sm:p-6 lg:p-8">
+            
+
+           <div className="mt- sm:mt- lg:mt-44">
+           <div className="text-white translate-y-11   transition-all group-hover:translate-y-0 group-hover:opacity-100  ">
+            <p className="text-gray-400 text-sm">jjjj</p>  
+            <p className="text-lg">kkkk</p>
+             <div className="mt-4 translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
+              
+               <button className="bg-green-700 w-full h-8 text-xl    text-white">
+                Donte
+               </button>
+             </div>
+            </div>
+           </div>
+         </div>     
+       </a></div>
+       </div>
+       </div>
+  </div>
+  
+  );
+}
+export default HomePage;
