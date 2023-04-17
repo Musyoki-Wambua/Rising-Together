f1a50fd55c21840fdf4dc74582d8723ff0902a36
tony555t <tonytullah764@gmail.com>
2023-04-17 13:32:32 +0300

    controller actions

--- backend/app/controllers/reviews_controller.rb
+++ backend/app/controllers/reviews_controller.rb
@@ -1,2 +1,34 @@
 class ReviewsController < ApplicationController
-end
+    def index
+      reviews = Review.all
+      render json: reviews,only: [:comments,:ratings]
+    end
+  
+    def show
+      review=Review.find(params[:id])
+      render json: review,only: [:comments,:ratings]
+    end
+  
+    def create
+      review = Review.new(review_params)
+  
+      if review.save
+
+        render json: review, status: :created
+      else
+        render json: review.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def delete
+      review = Review.find(params[:id])
+      review.delete
+      head :no_content
+    end
+  
+    private
+  
+    def review_params
+      params.require(:review).permit(:rating, :comment, :reviewer_name)
+    end
+  end
