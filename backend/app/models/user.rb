fa0214f30da159843752826bbfaed7e7e8520845
david <davmwa90@gmail.com>
2023-04-24 11:24:01 +0300

    created and updated user.rb

---
+++ backend/app/models/user.rb
@@ -0,0 +1,29 @@
+class User < ApplicationRecord
+  has_secure_password 
+
+  #Defining relationships 
+      has_one :profile 
+      has_many :organizations, through: :donations
+      has_many :donations 
+      has_many :reviews 
+
+  #Validations
+      validates :email, presence: true, uniqueness: true 
+      validates :password, length: {in: 5..10}
+      validates :password_confirmation, presence: true
+      # validates :role, inclusion: { in: %w[user], message: "Only users can sign up" }
+      
+  #Defining constants for role values
+    ROLES = {
+      admin: 'admin',
+      user: 'user'
+    }.freeze
+  
+  #Validate that role value is one of the defined constants
+    validates :role, inclusion: { in: ROLES.values }   
+  
+end
+
+
+
+
