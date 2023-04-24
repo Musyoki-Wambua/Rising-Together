59cd17d1273f28a7227e715162ad72bbcf217d09
david <davmwa90@gmail.com>
2023-04-24 11:18:33 +0300

    updated the organization controller

--- backend/app/controllers/organizations_controller.rb
+++ backend/app/controllers/organizations_controller.rb
@@ -1,51 +1,68 @@
 class OrganizationsController < ApplicationController
-# load_and_authorize_resource :organization
- 
-#Get a list of all organizations
-def index 
-    render json: Organization.all 
-end
-
-#Get a specific organization/:id
-def show 
-    organization = Organization.find_by(id: params[:id])
-    if organization 
-        render json: organization, status: :ok
-    else 
-        render json: { errors: "Organization not found!"}, status: :not_found
+    # load_and_authorize_resource :organization
+     
+    #Get a list of all organizations
+    def index 
+        render json: Organization.all 
     end
-end
-
-#Create an organization 
-def create 
-    organization = Organization.create(organization_params)
-    if organization.valid? 
-        render json: organization, status: :ok 
-    else
-        render json: { errors: organization.errors.full_messages}, status: :unprocessable_entity 
+    
+    #Get a specific organization/:id
+    def show 
+        organization = Organization.find_by(id: params[:id])
+        if organization 
+            render json: organization, status: :ok
+        else 
+            render json: { error: "Organization not found!"}, status: :not_found
+        end
     end
-end
-
-#Update an organization/:id
-def update 
-    organization = Organization.find_by(id: params[:id])
-    if organization.valid? 
-        organization.update(organization_params)
-        render json: organization, status: :ok
-    else 
-    render json: { errors: "Organization not found!"}, status: :not_found
+    
+    #Create an organization
+    def create 
+        if current_user.role == 'admin'
+          organization = Organization.create(organization_params)
+          if organization.valid? 
+            render json: organization, status: :ok 
+          else
+            render json: { errors: organization.errors.full_messages }, status: :unprocessable_entity 
+          end
+        else
+          render json: { error: "Access Denied!" }, status: :unauthorized
+        end
+      end
+      
+    #Update an organization/:id/  
+    def update 
+        organization = Organization.find_by(id: params[:id])
+        if organization
+          if current_user.role == 'admin' 
+            if organization.update(organization_params)
+              render json: organization, status: :ok
+            else
+              render json: { error: "Organization not updated!" }, status: :unprocessable_entity
+            end
+          else
+            render json: { error: "Access Denied!" }, status: :unauthorized
+          end
+        else 
+          render json: { error: "Organization not found!"}, status: :not_found
+        end
+      end
+      
+    #Delete an organization/:id
+    def destroy
+        organization = Organization.find_by(id: params[:id])
+        if current_user.role == 'admin' # Check if current user has admin role
+          organization.delete
+          render json: {message: "Organization successfully deleted!"}, status: :ok
+        else
+          render json: { error: "Access Denied!" }, status: :unauthorized
+        end
+      end
+      
+      
+    private 
+    def organization_params 
+        params.permit(:name, :description, :location, :year_established, :image_url, :video_url, :info, :contact, :user_id)
     end
-end
-
-#Delete an organization/:id
-def destroy
-    organization=Organization.find_by(id: params[:id])
-    organization.delete 
-    head :no_content
-end
-
-private 
-def organization_params 
-    params.permit(:name, :description, :location, :year_established, :image_url, :video_url, :info, :contact, :user_id)
-end
-end
+    end
+    
