9ade49c5e3062ca2bb005c2961636ef4c99874a2
febiasm <trevorfebias@gmail>
2023-05-02 10:43:36 +0300

    added loader

--- frontend/src/components/OrganizationDetails.js
+++ frontend/src/components/OrganizationDetails.js
@@ -27,22 +27,19 @@ function OrganizationDetails() {
 
   if (isLoading) {
     return (
-      <body class="w-full h-full bg-gray-900 text-white grid grid-cols-1 grid-rows-1 place-items-center">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="50"
-          height="50"
-          fill="currentColor"
-          class="bi bi-arrow-repeat animate-spin"
-          viewBox="0 0 16 16"
-        >
-          <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z" />
-          <path
-            fill-rule="evenodd"
-            d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"
-          />
-        </svg>
-      </body>
+      <div class=" bg-gray-900 flex items-center justify-center w-full h-full">
+      <div class="flex justify-center items-center space-x-1 text-sm  text-gray-700">
+         
+            <svg fill='none' class="w-6 h-6 animate-spin" viewBox="0 0 32 32" xmlns='http://www.w3.org/2000/svg'>
+              <path clip-rule='evenodd'
+                d='M15.165 8.53a.5.5 0 01-.404.58A7 7 0 1023 16a.5.5 0 011 0 8 8 0 11-9.416-7.874.5.5 0 01.58.404z'
+                fill='currentColor' fill-rule='evenodd' />
+            </svg>
+    
+         
+        <div className="text-white text-2xl">Loading ...</div>
+      </div>
+    </div>
     );
   }
 
